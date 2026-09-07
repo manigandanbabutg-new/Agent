ed67115e9a0b002dc0b41edb9ff34f9afcfe4279
manigandanbabutg-new <manigandanbabu.tg@gmail.com>
2026-09-07 21:38:04 +0530

    Refactor Flask app initialization
    Refactor create_app function to simplify initialization and remove unused code.

--- app/__init__.py
+++ app/__init__.py
@@ -1,27 +1,12 @@
-import os
 
 from flask import Flask, render_template
-from flask_cors import CORS
 
 from app.youtube import youtube_bp
 
 
 def create_app():
-    base_dir = os.path.dirname(
-        os.path.dirname(os.path.abspath(__file__))
-    )
 
-    template_dir = os.path.join(
-        base_dir,
-        "templates"
-    )
-
-    app = Flask(
-        __name__,
-        template_folder=template_dir
-    )
-
-    CORS(app)
+    app = Flask(__name__)
 
     app.register_blueprint(
         youtube_bp,
@@ -30,16 +15,8 @@ def create_app():
 
     @app.route("/")
     def home():
-        return render_template("index.html")
-
-    @app.route("/health")
-    def health():
-        return {
-            "success": True,
-            "message": "Nova AI is running"
-        }
+        return render_template(
+            "index.html"
+        )
 
     return app
-
-
-app = create_app()
